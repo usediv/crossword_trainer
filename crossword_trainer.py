315e2a320b4274156e899764dcb95625e25296b9
Chris Wilson <usediv@gmail.com>
2019-06-16 04:24:43 -0400

    Scraping complete
    Seems to be working well now. Had to squash some bugs and CSV writing was a pain in the ass for some reason but all seems good. Glad I erred on the side of caution running the script with small date ranges to begin as it's pretty intensive, don't want to crash their servers. Will probably want to add the ability to retrieve only specific days (eg Saturday) to minimize impact for them. Won't be hard as I'm already getting day values. Now what to do with all this data...

--- crossword_trainer.py
+++ crossword_trainer.py
@@ -1,46 +1,54 @@
 from bs4 import BeautifulSoup
 import requests, datetime, csv, os
 
+with open('crossword_training_data.csv', 'w') as csv_file:
+    fieldnames = ['day','clue','answer','length','date']
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
 
 print('Initialising dates...')
 
-# set up dates
+startDate = datetime.date(2019,6,1)
 today = datetime.date.today()
-day = today.weekday()
-splitDate = str(today).split('-')
-date = str(int(splitDate[1])) + '/' + splitDate[2] + '/' + splitDate[0]
 
-# set URL
-url = f'https://www.xwordinfo.com/Crossword?date={date}'
-r = requests.get(url)
+date = startDate
 
-# soupify webpage data
-soup = BeautifulSoup(r.text, 'lxml')
+while date <= today:
+    day = date.weekday()
+    splitDate = str(date).split('-')
+    formattedDate = str(int(splitDate[1])) + '/' + splitDate[2] + '/' + splitDate[0]
 
-# csv stuff
-# os.chdir('/Users/chriswilson/Documents/GitHub/crossword_trainer')
-csv_file = open('crossword_training_data.csv', 'w')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['day','clue','answer','length'])
+    url = f'https://www.xwordinfo.com/Crossword?date={formattedDate}'
+    r = requests.get(url)
 
-# check crossword clues and answers
-print('Checking for clues...')
+    # soupify webpage data
+    soup = BeautifulSoup(r.text, 'lxml')
 
-try:
+    print('Getting clues and answers for ' + str(date))
+
+    clueCount = 0
+
     # iterate through both blocks of clues and answers (across and down)
     for numClues in soup.find_all('div', class_='numclue'):
         # break out individual divs from blocks
-        clues = numClues.findChildren('div' , recursive=False)
+        clues = numClues.findChildren('div', recursive=False)
         # grab just the clues and answers, ditch the numbers and format
-        clueCount = 0
         for clue in clues[1::2]:
             clueCount += 1
-            answer = []
             clue = clue.text.split(' : ')
-            csv_writer.writerow([day,clue[0],clue[1],len(clue[1])])
+            # create/set dictionary values
+            info = {}
+            info.update({'day': day, 'clue': clue[0], 'answer': clue[1], 'length': len(clue[1]), 'date': date})
+            # open csv and append dictionary as row
+            with open('crossword_training_data.csv', 'a') as csv_file:
+                csv_writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
+                csv_writer.writerow(info)
+
     print(str(clueCount) + ' clues and answers found')
+    date += datetime.timedelta(days=1)
 
-    csv_file.close()
-
-except:
-    print('Sorry, clues not found')
+# reading data with DictReader
+# with open('crossword_training_data.csv', 'r') as csv_file:
+#     csv_reader = csv.DictReader(csv_file,fieldnames=fieldnames)
+#     for item in csv_reader:
+#         print(item['answer'])
