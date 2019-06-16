56f2897c720a9d8ecfccb6f609ae0d993475b496
Chris Wilson <usediv@gmail.com>
2019-06-16 00:35:06 -0400

    Date formatting working
    Only working for today but inserting dates into URLs for answer site. Will work on CSV population before implementing iteration through dates to limit size of data set and server requests while testing.

--- crossword_trainer.py
+++ crossword_trainer.py
@@ -1,20 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 
-r = requests.get('https://www.xwordinfo.com/Crossword?date=6/15/2019')
+today = datetime.date.today()
+day = today.weekday()
+splitDate = str(today).split('-')
 
-day = 'Saturday'
 
+url = 'https://www.xwordinfo.com/Crossword?date=' + str(int(splitDate[1])) + '/' + splitDate[2] + '/' + splitDate[0]
+
+r = requests.get(url)
+
+# soupify
 soup = BeautifulSoup(r.text, 'lxml')
 
+# iterate through both blocks of clues and answers (across and down)
 for numClues in soup.find_all('div', class_='numclue'):
+    # break out individual divs from blocks
     clues = numClues.findChildren('div' , recursive=False)
+    # grab just the clues and answers, ditch the numbers and format
     for clue in clues[1::2]:
         answer = []
-        clue = clue.text.split(': ')
-        answer.append(day)
-        answer.append(clue[0])
-        answer.append(clue[1])
-        answer.append(len(clue[1]))
+        clue = clue.text.split(' : ')
+        answer.extend([day,clue[0],clue[1],len(clue[1])])
         print(answer)
